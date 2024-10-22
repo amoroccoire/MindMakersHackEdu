a2fadc856a9c5e1fb5fbffb259adb45ad9eacc49
amoroccoire <amoroccoire@unsa.edu.pe>
2024-10-22 11:37:52 -0500

    agregando servicio para gestionar el token

--- Backend/dashboard-api/src/main/java/com/mindmakers/dashboard_api/auth/TokenService.java
+++ Backend/dashboard-api/src/main/java/com/mindmakers/dashboard_api/auth/TokenService.java
@@ -1,4 +1,71 @@
 package com.mindmakers.dashboard_api.auth;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.mindmakers.dashboard_api.user.entities.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
 public class TokenService {
+    @Value("${api.security.secret}")
+    private String apiSecret;
+    public String generarToken(Usuario usuario) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            return JWT.create()
+                    .withIssuer("apiHack")
+                    .withSubject(usuario.getGmail())
+                    .withClaim("id", usuario.getId())
+                    .withExpiresAt(generarFechaExpiracion())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException();
+        }
+    }
+
+    public boolean validarToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("apiHack")
+                    .build(); // Verificador del token
+            verifier.verify(token);
+            return true;
+        } catch (JWTVerificationException exception) {
+            return false; // El token no es válido
+        }
+    }
+
+    public String getSubject(String token) {
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            verifier = JWT.require(algorithm)
+                    // specify any specific claim validations
+                    .withIssuer("apiHack")
+                    // reusable verifier instance
+                    .build()
+                    .verify(token);
+            verifier.getSubject();
+        } catch (JWTVerificationException exception) {
+            // Invalid signature/claims
+        }
+        if(verifier.getSubject() == null) {
+            throw new RuntimeException("Verifier invalido");
+        }
+        return verifier.getSubject();
+    }
+
+    private Instant generarFechaExpiracion() {
+        return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-05:00"));
+    }
 }
