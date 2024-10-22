6ba684981703bfd9b6fa6af5ce7e85d04c47f9f6
amoroccoire <amoroccoire@unsa.edu.pe>
2024-10-22 11:38:27 -0500

    configurando manejador de rutas

--- Backend/dashboard-api/src/main/java/com/mindmakers/dashboard_api/auth/SecurityFilter.java
+++ Backend/dashboard-api/src/main/java/com/mindmakers/dashboard_api/auth/SecurityFilter.java
@@ -1,4 +1,52 @@
 package com.mindmakers.dashboard_api.auth;
 
-public class SecurityFilter {
+import com.mindmakers.dashboard_api.user.adaptadores.UserDetailsImpl;
+import com.mindmakers.dashboard_api.user.entities.Usuario;
+import com.mindmakers.dashboard_api.user.repositories.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            var token = authHeader.replace("Bearer ", "");
+            System.out.println(token);
+            System.out.println(tokenService.getSubject(token));
+            if (tokenService.validarToken(token)) {
+                var subject = tokenService.getSubject(token);
+                if (subject != null) {
+                    Optional<Usuario> usuario = usuarioRepository.findByGmail(subject);
+                    if (usuario.get() == null) {
+                        new UsernameNotFoundException("Usuario no encontrado");
+                        return;
+                    }
+                    var userDetails = new UserDetailsImpl(usuario.get());
+                    var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 }
