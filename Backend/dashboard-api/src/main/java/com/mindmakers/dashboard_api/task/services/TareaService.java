2d533ffc0b6c698ba4dd8f9d20118eff1abff363
amoroccoire <amoroccoire@unsa.edu.pe>
2024-10-22 11:43:08 -0500

    servicios para la entidad tarea

--- Backend/dashboard-api/src/main/java/com/mindmakers/dashboard_api/task/services/TareaService.java
+++ Backend/dashboard-api/src/main/java/com/mindmakers/dashboard_api/task/services/TareaService.java
@@ -1,4 +1,15 @@
 package com.mindmakers.dashboard_api.task.services;
 
+import com.mindmakers.dashboard_api.task.entities.Tarea;
+import com.mindmakers.dashboard_api.task.repositories.TareaRepository;
+import com.mindmakers.dashboard_api.task.services.dto.TareaDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TareaService {
+
+
 }
