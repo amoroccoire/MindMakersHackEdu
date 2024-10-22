205b16f1d0d167958ebbf04d772ae3d0a7222d4f
Ronald Romario Gutierrez Arratia <113565299+Ronald-Gutierrez@users.noreply.github.com>
2024-10-22 16:26:41 -0500

    Modificando plantilla

--- Frontend/package/src/app/authentication/login/page.tsx
+++ Frontend/package/src/app/authentication/login/page.tsx
@@ -55,7 +55,7 @@ const Login2 = () => {
                     color="textSecondary"
                     mb={1}
                   >
-                    Your Social Campaigns
+                  ¡Bienvenido!
                   </Typography>
                 }
                 subtitle={
@@ -70,7 +70,7 @@ const Login2 = () => {
                       variant="h6"
                       fontWeight="500"
                     >
-                      New to Spike?
+                      ¿Eres nuevo en Innova Class?
                     </Typography>
                     <Typography
                       component={Link}
@@ -81,7 +81,7 @@ const Login2 = () => {
                         color: "primary.main",
                       }}
                     >
-                      Create an account
+                      Registrate ahora
                     </Typography>
                   </Stack>
                 }
