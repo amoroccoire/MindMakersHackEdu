a63540e3c48e1cc25ed5f3afc06f3656c9de0302
amoroccoire <amoroccoire@unsa.edu.pe>
2024-10-22 11:37:05 -0500

    configurando repositorio para el ussuario, antes usaba la implmentacion de usuario Details

--- Backend/dashboard-api/src/main/java/com/mindmakers/dashboard_api/user/repositories/UsuarioRepository.java
+++ Backend/dashboard-api/src/main/java/com/mindmakers/dashboard_api/user/repositories/UsuarioRepository.java
@@ -1,4 +1,14 @@
 package com.mindmakers.dashboard_api.user.repositories;
 
-public class UsuarioRepository {
+import com.mindmakers.dashboard_api.user.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByGmail(String correo);
 }
