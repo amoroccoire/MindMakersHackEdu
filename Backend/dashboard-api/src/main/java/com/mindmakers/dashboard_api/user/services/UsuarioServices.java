e0715199eba593baf55a3ae167b93fc57cd23710
amoroccoire <amoroccoire@unsa.edu.pe>
2024-10-22 11:44:02 -0500

    crando ervicio para registro de ussuarios

--- Backend/dashboard-api/src/main/java/com/mindmakers/dashboard_api/user/services/UsuarioServices.java
+++ Backend/dashboard-api/src/main/java/com/mindmakers/dashboard_api/user/services/UsuarioServices.java
@@ -1,4 +1,34 @@
 package com.mindmakers.dashboard_api.user.services;
 
+import com.mindmakers.dashboard_api.user.dto.UsuarioRegisterDto;
+import com.mindmakers.dashboard_api.user.entities.Usuario;
+import com.mindmakers.dashboard_api.user.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+@Service
 public class UsuarioServices {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public void registrarUsuario(UsuarioRegisterDto usuarioRegisterDto) {
+        Optional<Usuario> userFound = usuarioRepository.findByGmail(usuarioRegisterDto.email());
+        if (userFound.get() == null) {
+            throw new RuntimeException("El correo ya está registrado.");
+        }
+        String cifrado = passwordEncoder.encode(usuarioRegisterDto.password());
+        Usuario usuario = Usuario.builder()
+                .id(null)
+                .username(usuarioRegisterDto.username())
+                .password(cifrado)
+                .gmail(usuarioRegisterDto.email())
+                .semestre(usuarioRegisterDto.semestre())
+                .build();
+        usuarioRepository.save(usuario);
+    }
 }
