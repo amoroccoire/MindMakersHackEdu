f4a909f76d2ce3f309e9ddd039755c0d1fa4285d
amoroccoire <amoroccoire@unsa.edu.pe>
2024-10-22 11:40:22 -0500

    manejador del username para springsecurity

--- Backend/dashboard-api/src/main/java/com/mindmakers/dashboard_api/auth/AuthenticationService.java
+++ Backend/dashboard-api/src/main/java/com/mindmakers/dashboard_api/auth/AuthenticationService.java
@@ -1,4 +1,29 @@
 package com.mindmakers.dashboard_api.auth;
 
-public class AuthenticationService {
+import com.mindmakers.dashboard_api.user.adaptadores.UserDetailsImpl;
+import com.mindmakers.dashboard_api.user.entities.Usuario;
+import com.mindmakers.dashboard_api.user.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Usuario> usuario = usuarioRepository.findByGmail(username);
+
+        if (usuario.get() == null) {
+            new UsernameNotFoundException("Usuario no encontrado");
+        }
+
+        return new UserDetailsImpl(usuario.get());
+    }
 }
