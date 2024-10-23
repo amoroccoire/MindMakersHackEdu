162fae8a6a2fd087b44c593de35eb8484b18fdb1
amoroccoire <amoroccoire@unsa.edu.pe>
2024-10-23 05:57:38 -0500

    agregnado schemas para Cursos, Usuario, tabalaUsuario y tareaDocente

--- Backend/app/schemas.py
+++ Backend/app/schemas.py
@@ -34,3 +34,92 @@ class UsuarioCreate(BaseModel):
     gmail: str
     semestre: int
     carreraProfesional: str
+
+
+# Esquema para la respuesta, incluyendo el id
+class UsuarioResponse(BaseModel):
+    id: int
+    username: str
+    gmail: str
+    semestre: int
+    carreraProfesional: str
+
+    class Config:
+        orm_mode = True
+
+
+#### CURSO ####
+class CursoBase(BaseModel):
+    docente: str
+    nombre: str
+    creditos: int
+
+class CursoCreate(BaseModel):
+    docente: str
+    nombre: str
+    creditos: int
+
+class CursoUpdate(CursoBase):
+    docente: str
+    nombre: str
+    creditos: int
+
+class CursoResponse(BaseModel):
+    id: int
+    docente: str
+    nombre: str
+    creditos: int
+
+    class Config:
+        orm_mode = True
+
+### CURSO ###
+
+### TAREA DOCENTE ###
+class TareaDocenteBase(BaseModel):
+    idCurso: int
+    descripcion: str
+    fecha_fin: str  # Formato dd/mm/yy HH:MM
+
+class TareaDocenteCreate(TareaDocenteBase):
+    idCurso: int
+    descripcion: str
+    fecha_fin: str  # Formato dd/mm/yy HH:MM
+
+class TareaDocenteUpdate(TareaDocenteBase):
+    pass
+
+class TareaDocenteResponse(TareaDocenteBase):
+    id: int
+    fecha_inicio: datetime  # Modificado para ser devuelto como string
+    fecha_fin: datetime
+
+    class Config:
+        from_attributes = True
+        json_encoders = {
+            datetime: lambda v: v.strftime('%d/%m/%y %H:%M'),  # Convertir datetime a string en formato dd/mm/yy HH:MM
+        }
+### FIN TAREA DOCENTE ###
+
+### TAREA USUARIO ###
+class TareaUsuarioBase(BaseModel):
+    idTareaDocente: int
+    idUsuario: int
+    isCompleted: bool
+
+class TareaUsuarioUpdate(BaseModel):
+    isCompleted: bool
+    calificacion: Optional[int]
+
+class TareaUsuarioResponse(BaseModel):
+    id: int
+    idTareaDocente: int
+    idUsuario: int
+    isCompleted: bool
+    fechaSubmit: Optional[datetime]
+    calificacion: Optional[int]
+
+    class Config:
+        from_attributes = True  # En Pydantic v2
+
+### FIN TAREA DOCENTE ###
